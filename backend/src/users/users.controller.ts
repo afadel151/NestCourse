cf42be418f7113a61ef38d0b6062defabcbbccfd
afadel151 <af8356207@gmail.com>
2025-03-10 01:18:20 +0100

    creating User decorator

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { User } from 'src/decorators/user.decorator';
 
 @Controller('users')
 export class UsersController {
 
     @UseGuards(AuthGuard)
     @Get('profile')
-    getProfile(@Req() req) {
-        return req.user;
+    getProfile(@User() user : any) {
+        return user;
     }
 }
