a3670652a2bcaeff16ba6e799567ee3fbb0d472d
afadel151 <af8356207@gmail.com>
2025-03-12 02:52:48 +0100

    getUserApplications endpoint that returns job applications made by current user

--- backend/src/applications/applications.controller.ts
+++ backend/src/applications/applications.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApplicationsService } from './applications.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { User } from 'src/decorators/user.decorator';
+@UseGuards(AuthGuard)
 @Controller('applications')
-export class ApplicationsController {}
+export class ApplicationsController {
+    constructor(private applicationService: ApplicationsService) {}
+    @Get('user')
+    getUserApplications(@User() user: any) {
+        return this.applicationService.getUserApplications(user);
+    }
+}
--- backend/src/applications/applications.service.ts
+++ backend/src/applications/applications.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ApplicationsService {}
+export class ApplicationsService {
+    constructor(private prismaService: PrismaService) { }
+    async getUserApplications(user: any) {
+        const Applications = await this.prismaService.application.findMany({
+            where: {
+                OR: [
+                    { freelancerId: user.sub },
+                ]
+            },
+            select: {
+                coverLetter: true,
+                status: true,
+                createdAt: true,
+                job: {
+                    select: {
+                        title: true,
+                        description: true,
+                        budget: true,
+                        status: true,
+                    }
+                },
+                
+            }
+        });
+        if (!Applications) {
+            throw new Error('User not found');
+        }
+        return Applications;
+    }
+}
